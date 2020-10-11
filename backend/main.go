3eccbd42818a1db1d3b12d4fb5e313a7da6874ab
Dictor <kimdictor@gmail.com>
2020-10-11 04:51:18 +0000

    vue-router용 정적 파일 서빙 로직 수정
    * 추가: 정적 파일이 존재하지 않으면 404대신 index.html을 서빙
    * 추가: 관련 패키지 추가

--- backend/main.go
+++ backend/main.go
@@ -1,15 +1,69 @@
 package main
 
 import (
+	"net/http"
+
 	elogrus "github.com/dictor/echologrus"
+	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+)
+
+// CustomValidator is struct validator for request input
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+// Validate is just renamed function of struct validate method
+func (cv *CustomValidator) Validate(i interface{}) error {
+	return cv.validator.Struct(i)
+}
+
+var (
+	// Logger is global logger reference to Echo object's logger
+	Logger       elogrus.EchoLogger
+	gitHash      string = "N/A"
+	gitTag       string = "N/A"
+	buildDate    string = "N/A"
+	displayDebug string = "false"
 )
 
 func main() {
 	e := echo.New()
-	elogrus.Attach(e)
+	Logger = elogrus.Attach(e)
+	e.Validator = &CustomValidator{validator: validator.New()}
+	Logger.Infof("leave-it-us backend %s (%s) : %s\n", gitTag, gitHash, buildDate)
 
-	e.Static("/", "static")
+	if displayDebug == "true" {
+		Logger.SetLevel(log.DEBUG)
+		Logger.Debugln("run in debug mode")
+	}
 
+	e.GET("/api/tag", func(c echo.Context) error {
+		tags := []Tag{}
+		if ListTable(&tags) != nil {
+			return c.NoContent(http.StatusInternalServerError)
+		}
+		return c.JSON(http.StatusOK, &tags)
+	})
+	e.POST("/api/tag", func(c echo.Context) error {
+		tag := Tag{}
+		if err := c.Bind(&tag); err != nil {
+			e.Logger.Info(err)
+			return c.NoContent(http.StatusBadRequest)
+		}
+		if err := c.Validate(tag); err != nil {
+			e.Logger.Info(err)
+			return c.NoContent(http.StatusBadRequest)
+		}
+		CreateRow(&tag)
+		return c.NoContent(http.StatusOK)
+	})
+
+	//static resource
+	e.GET("/*", VueRouterStatic("static"))
+
+	SetDBHander(TestDBHander())
+	Migrate()
 	e.Logger.Fatal(e.Start(":80"))
 }
---
+++ backend/static.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"os"
+
+	"github.com/kennygrant/sanitize"
+	"github.com/labstack/echo/v4"
+)
+
+//VueRouterStatic is static file serving handler for vue-router
+func VueRouterStatic(root string) func(c echo.Context) error {
+	return func(c echo.Context) error {
+		u := c.Request().URL
+		path := sanitize.Path(u.EscapedPath())
+		c.Logger().Debugf("inspect: %s > %s\n", u, path)
+		var servePath string
+
+		if fileExists(root + path) {
+			servePath = root + path
+		} else {
+			servePath = root + "/index.html"
+		}
+		c.Logger().Debug(servePath)
+		return c.File(servePath)
+	}
+}
+
+func fileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
