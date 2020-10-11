a3783377c001320158b5a7bf1b3bd918d333cb27
Dictor <kimdictor@gmail.com>
2020-10-11 08:32:39 +0000

    삭제 함수 추가

--- backend/database.go
+++ backend/database.go
@@ -20,14 +20,22 @@ func SetDBHander(h *gorm.DB) {
 	currentDatabase = h
 }
 
-// CreateRow create serveral rows, row can be every type of pointer or object
+// CreateRow create serveral rows, row can be every type of pointer
 func CreateRow(row interface{}) error {
 	return currentDatabase.Create(row).Error
 }
 
-// ListTable return all rows on table, table is decided by result parameter's reflect. And returing data is contained to it
+// ListTable return all rows on table, table is decided by result parameter's reflect. And returing data is contained to it and it musb be every type of pointer.
 func ListTable(result interface{}) error {
 	return currentDatabase.Find(result).Error
+}
+
+func DeleteRow(row interface{}) error {
+	return currentDatabase.Delete(row).Error
+}
+
+func DeleteRowByKeys(model interface{}, keys interface{}) error {
+	return currentDatabase.Delete(model, keys).Error
 }
 
 // Migrate is migrate database with all models
