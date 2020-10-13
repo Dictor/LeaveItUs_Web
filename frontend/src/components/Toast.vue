b70e15879139211c4fb20636f0acb657fd66610c
Dictor <kimdictor@gmail.com>
2020-10-13 14:06:17 +0000

    토스트 컴포넌트 생성
    * msgs 속성을 bind하여 사용 가능
    * {msg:"", kind:""}와 같이 사용, kind 기본값 = info, 문자열만 넘어올시 문자열 표시

---
+++ frontend/src/components/Toast.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-container class="pa-2 mb-2 toast-parent">
+        <v-container v-for="(msg, i) in msgs" :key="i" class="pa-0 ma-0">
+            <v-alert :type="msg.kind ? msg.kind : 'info'" class="ma-1">
+                {{ !(msg.msg) ? msg: msg.msg }}
+            </v-alert>
+        </v-container>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: "Toast",
+    data: () => ({
+    }),
+    props: ["msgs"],
+    mounted() {
+        setInterval(() => {
+            const val = this.msgs;
+            if (!val) return;
+            const now = Date.now();
+            for (let i = 0; i < val.length; i++) {
+                if (val[i].created) {
+                    if (now - val[i].created > 2000) {
+                        val.splice(i, 1);
+                    }
+                } else {
+                    val[i].created = now;
+                }
+            }
+        }, 500);
+    }
+}
+</script>
+        
+<style scoped>
+    .toast-parent {
+        position: fixed;
+        bottom: 0;
+        right: 0;
+    }
+</style>
