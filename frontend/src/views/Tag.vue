1eef7eed4b79108227b25b4f07d9479505786b93
Dictor <kimdictor@gmail.com>
2020-10-13 14:07:29 +0000

    토스트 분리 및 코드 정리
    * 변경: 토스트를 별도 컴포넌트로 분리
    * 변경: listTag 함수에 딜레이 기능 추가
    * 변경: addDialog 취소 버튼 작동하지 않는 문제 수정

--- frontend/src/views/Tag.vue
+++ frontend/src/views/Tag.vue
@@ -1,15 +1,9 @@
 <template>
     <v-container fluid>
-        <v-container class="pa-2 mb-2 toast">
-            <v-container v-for="(error, i) in errors" v-bind:key="i" class="pa-0 ma-0">
-                <v-alert :type="error.kind" class="ma-1" v-if="error.visible">
-                    {{ error.msg }}
-                </v-alert>
-            </v-container>
-        </v-container>
+        <toast :msgs="msgs"/>
         <v-card class="pa-2 mb-2">
-            <v-btn key="" color="green" v-on:click="shoWAddDialog" dark large class="mr-1"><v-icon>mdi-plus</v-icon>태그 추가</v-btn>
-            <v-btn key="" color="error" v-on:click="deleteTag(); listTag();" large class="mr-1"><v-icon>mdi-delete</v-icon>태그 삭제</v-btn>
+            <v-btn key="" color="green" v-on:click="showAddDialog" dark large class="mr-1"><v-icon>mdi-plus</v-icon>태그 추가</v-btn>
+            <v-btn key="" color="error" v-on:click="deleteTag(); listTag(300);" large class="mr-1"><v-icon>mdi-delete</v-icon>태그 삭제</v-btn>
             <v-btn key="" color="blue"  v-on:click="listTag" large><v-icon>mdi-refresh</v-icon>새로고침</v-btn>
         </v-card>
         <v-card>
@@ -60,8 +54,8 @@
                     </v-card-text>
                     <v-card-actions>
                         <v-spacer></v-spacer>
-                        <v-btn color="blue darken-1" text @click="shoWAddDialog">취소</v-btn>
-                        <v-btn color="blue darken-1" text @click="addTag(); visibleAddDialog = false; listTag();">추가</v-btn>
+                        <v-btn color="blue darken-1" text @click="closeAddDialog">취소</v-btn>
+                        <v-btn color="blue darken-1" text @click="addTag(); closeAddDialog(); listTag();">추가</v-btn>
                     </v-card-actions>
                 </v-card>
         </v-dialog>
@@ -71,6 +65,7 @@
 
 <script>
 import axios from 'axios';
+import Toast from '../components/Toast.vue';
 
 export default {
     name: "Tag",
@@ -86,10 +81,13 @@ export default {
         selected: [],
         visibleAddDialog: false,
         addDialog: {},
-        errors: []
+        msgs: [],
       }),
+    components: {
+        Toast,
+    },
     methods: {
-        shoWAddDialog: function() {
+        showAddDialog: function() {
             this.addDialog = {
                 uid: "",
                 id: "",
@@ -98,46 +96,50 @@ export default {
             };
             this.visibleAddDialog = true;
         },
+        closeAddDialog: function() {
+            this.visibleAddDialog = false;
+        },
         addTag: function() {
             axios.post("./api/tag", this.addDialog)
                 .then(() => {
-                    this.addToast("추가 성공!", "success");
+                    this.msgs.push({msg: "추가 성공!", kind: "success"});
                 })
                 .catch((err) => {
-                    this.addToast(err, "error");
+                    this.msgs.push({msg: err, kind: "error"});
                 });
         },
         deleteTag: function() {
             const t = this.selected.map(x => x.uid);
             this.selected = [];
             if (t.length < 1) {
-                this.addToast("삭제할 태그를 지정하세요.", "error");
+                this.msgs.push({msg: "삭제할 태그를 지정하세요.", kind: "error"});
                 return;
             }
             //in axios 0.20, axios.delete not working with data option
             axios.request({data: {uids: t}, url: "./api/tag", method: 'delete'})
                 .then(() => {
-                    this.addToast("삭제 성공!", "success");
+                    this.msgs.push({msg: "삭제 성공!", kind: "success"});
                 })
                 .catch((err) => {
-                    this.addToast(err, "error");
+                    this.msgs.push({msg: err, kind: "error"});
                 });
         },
-        listTag: function() {
-            axios.get("./api/tag")
-                .then((res) => {
-                    this.tags = res.data;
-                    this.addToast("불러오기 성공!", "success");
-                })
-                .catch((err) => {
-                    this.addToast(err, "error");
-                });
-        },
-        addToast: function(msg, kind) {
-            let i = this.errors.push({msg: msg, visible: true, kind: kind});
-            setTimeout(() => {
-                this.errors[i-1].visible = false;
-            }, 2000);
+        listTag: function(delay) {
+            const req = () => {
+                axios.get("./api/tag")
+                    .then((res) => {
+                        this.tags = res.data;
+                        this.msgs.push({msg: "불러오기 성공!", kind: "success"});
+                    })
+                    .catch((err) => {
+                        this.msgs.push({msg: err, kind: "error"});
+                    });
+            }
+            if (!delay || delay === 0) {
+                req();
+            } else {
+                setTimeout(req, delay);
+            }
         },
         mounted() { 
             this.listTag();
@@ -147,9 +149,4 @@ export default {
 </script>
 
 <style scoped>
-    .toast {
-        position: fixed;
-        bottom: 0;
-        right: 0;
-    }
 </style>
