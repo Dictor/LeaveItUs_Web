ad1d05c9e0f93b209f87d6f44b2c817dea4e0049
Dictor <kimdictor@gmail.com>
2020-10-18 09:23:24 +0000

    병력 관리 UI 구현
    * 추가: 토스트, 데이터 테이블, Crud 로직

--- frontend/src/views/Personnel.vue
+++ frontend/src/views/Personnel.vue
@@ -1,11 +1,99 @@
 <template>
-    <p> 병력관리 입니다. </p>
+    <v-container fluid>
+        <toast :msgs="msgs"/>
+        <data-table
+            title="병력"
+            :columns="columns"
+            :rows="persons"
+            :dialog="dialog"
+            @create="addPerson"
+            @read="listPerson"
+            @update="updatePerson"
+            @delete="deletePerson"
+        >
+        </data-table>
+    </v-container>
 </template>
 
 <script>
+import axios from 'axios';
+import Toast from '../components/Toast.vue';
+import DataTable from '../components/DataTable.vue';
+
 export default {
-    name: "Tag",
+    name: "Personnel",
     data: () => ({
+        columns: [
+          { text: '군번', value: 'id'},
+          { text: '이름', value: 'name'},
+          { text: '부서', value: 'department'},
+        ],
+        persons: [],
+        dialog: [
+            [
+                {key: "id", label: "군번", required: true},
+                {key: "name", label: "이름", required: true},
+            ],
+            [
+                {key: "department", label: "부서", required: true},
+            ]
+        ],
+        msgs: {},
     }),
+    method: {
+        addPerson: (dialogData) => {
+            axios.post("./api/person", dialogData)
+                .then(() => {
+                    this.msgs.push({msg: "추가 성공!", kind: "success"});
+                })
+                .catch(this.handleError);
+        },
+        listPerson: (delay) => {
+            const req = () => {
+                axios.get("./api/person")
+                    .then((res) => {
+                        this.persons = res.data;
+                        this.msgs.push({msg: "불러오기 성공!", kind: "success"});
+                    })
+                    .catch(this.handleError);
+            }
+            if (!delay || delay === 0) {
+                req();
+            } else {
+                setTimeout(req, delay);
+            }
+        },
+        updatePerson: (dialogData) => {
+
+        },
+        deletePerson: (selected) => {
+            const t = selected.map(x => x.id);
+            if (t.length < 1) {
+                this.msgs.push({msg: "삭제할 병력를 지정하세요.", kind: "error"});
+                return;
+            }
+            //in axios 0.20, axios.delete not working with data option
+            axios.request({data: {id: t}, url: "./api/person", method: 'delete'})
+                .then(() => {
+                    this.msgs.push({msg: "삭제 성공!", kind: "success"});
+                })
+                .catch(this.handleError);
+        },
+        handleError: function(error) {
+            if (error.response) {
+                if (error.response.data.msg) {
+                    this.msgs.push({msg: "서버의 실패 응답 : " + error.response.data.msg + "(" + error.response.status + ")", kind: "error"});
+                } else {
+                    this.msgs.push({msg: error + " (" + error.response.status + ")", kind: "error"});
+                }
+            }
+            else if (error.request) {
+                this.msgs.push({msg: "응답 수신 실패", kind: "error"});
+            }
+            else {
+                this.msgs.push({msg: "요청 실패", kind: "error"});
+            }
+        },
+    }
 }
 </script>
