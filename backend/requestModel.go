6d2bed140adf1cfc6d45af2eca881488add79785
Dictor <kimdictor@gmail.com>
2020-10-18 09:45:47 +0000

    주석 및 요청 정의 추가
    * 추가: 함수 주석
    * 추가: Person 모델 삭제 요청 정의

--- backend/requestModel.go
+++ backend/requestModel.go
@@ -1,7 +1,13 @@
 package main
 
 type (
+	// TagDeleteRequest is definition of DELETE tag request's body
 	TagDeleteRequest struct {
 		UIDs []string `json:"uids" validate:"required,printascii"`
+	}
+
+	// PersonDeleteRequest is definition of DELETE tag request's body
+	PersonDeleteRequest struct {
+		IDs []string `json:"ids" validate:"required,printascii"`
 	}
 )
