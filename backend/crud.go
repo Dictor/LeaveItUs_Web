9dea4ad2cbd16fac7cca093a0061c22c8ccc2e54
Dictor <kimdictor@gmail.com>
2020-10-20 12:09:03 +0000

    Change all CrudModel's Instance function returning pointer

--- backend/crud.go
+++ backend/crud.go
@@ -2,55 +2,57 @@ package main
 
 import (
 	"net/http"
+	"reflect"
 
 	"github.com/labstack/echo/v4"
 )
 
 type CrudModel interface {
-	Instance() interface{}
-	DeleteRequestInstance() interface{}
-	DeleteRequestKey(interface{}) interface{}
-	Slice() interface{}
+	InstancePointer() interface{}
+	DeleteInstancePointer() interface{}
+	DeleteKey(interface{}) interface{}
+	SlicePointer() interface{}
 }
 
 func ReadHandler(model CrudModel) func(c echo.Context) error {
 	return func(c echo.Context) error {
-		slice := model.Slice()
-		if ListTable(&slice) != nil {
+		slice := model.SlicePointer()
+		c.Logger().Debug(reflect.TypeOf(slice).String())
+		if ListTable(slice) != nil {
 			return c.NoContent(http.StatusInternalServerError)
 		}
-		return c.JSON(http.StatusOK, &slice)
+		return c.JSON(http.StatusOK, slice)
 	}
 }
 
-func CreateHandler(model CrudModel, logger echo.Logger) func(c echo.Context) error {
+func CreateHandler(model CrudModel) func(c echo.Context) error {
 	return func(c echo.Context) error {
-		instance := model.Instance()
-		if err := c.Bind(&instance); err != nil {
-			logger.Info(err)
+		instance := model.InstancePointer()
+		if err := c.Bind(instance); err != nil {
+			c.Logger().Info(err)
 			return c.NoContent(http.StatusBadRequest)
 		}
 		if err := c.Validate(instance); err != nil {
-			logger.Info(err)
+			c.Logger().Info(err)
 			return c.NoContent(http.StatusBadRequest)
 		}
-		return ParseSQLErrorToResponse(CreateRow(&instance), c)
+		return ParseSQLErrorToResponse(CreateRow(instance), c)
 	}
 }
 
-func DeleteHandler(model CrudModel, logger echo.Logger) func(c echo.Context) error {
+func DeleteHandler(model CrudModel) func(c echo.Context) error {
 	return func(c echo.Context) error {
-		dinstance := model.DeleteRequestInstance()
-		if err := c.Bind(&dinstance); err != nil {
-			logger.Info(err)
+		dinstance := model.DeleteInstancePointer()
+		if err := c.Bind(dinstance); err != nil {
+			c.Logger().Info(err)
 			return c.NoContent(http.StatusBadRequest)
 		}
 		if err := c.Validate(dinstance); err != nil {
-			logger.Info(err)
+			c.Logger().Info(err)
 			return c.NoContent(http.StatusBadRequest)
 		}
-		instance := model.Instance()
-		keys := model.DeleteRequestKey(dinstance)
-		return ParseSQLErrorToResponse(DeleteRowByKeys(&instance, &keys), c)
+		instance := model.InstancePointer()
+		keys := model.DeleteKey(dinstance)
+		return ParseSQLErrorToResponse(DeleteRowByKeys(instance, keys), c)
 	}
 }
