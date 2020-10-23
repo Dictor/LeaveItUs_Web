e0c73e6db7b6964eea8cf35038d2bca10d5e0688
Dictor <kimdictor@gmail.com>
2020-10-23 14:11:27 +0000

    Tag와 Locker 모델의 gorm 태그 수정
    * 추가: 외래키가 정상적으로 동작하도록 references 태그 추가
    * 변경: gorm.Model을 json 마샬시 무시하도록 설정

--- backend/model.go
+++ backend/model.go
@@ -9,12 +9,12 @@ import (
 type (
 	// Tag is definition of NFC tag which is attached on behind of device like smartphone
 	Tag struct {
-		UID        string `json:"uid" validate:"required,printascii" gorm:"primaryKey,unique"` // tag's unique id
-		ID         string `json:"id" validate:"required,printascii"`                           // tag's managing id
-		AssigneeID string `json:"assignee_id" validate:"required,printascii"`                  // id of person who assigned this tag (same with device owner)
-		DeviceID   string `json:"device_id" validate:"required,printascii"`                    // id of device which this tag is attached
-		LockerUID  string `json:"locker_uid" validate:"required,printascii"`
-		gorm.Model        // model for managing record's crud datetime
+		UID        string     `json:"uid" validate:"required,printascii" gorm:"primaryKey,unique"` // tag's unique id
+		ID         string     `json:"id" validate:"required,printascii"`                           // tag's managing id
+		AssigneeID string     `json:"assignee_id" validate:"required,printascii"`                  // id of person who assigned this tag (same with device owner)
+		DeviceID   string     `json:"device_id" validate:"required,printascii"`                    // id of device which this tag is attached
+		LockerUID  string     `json:"locker_uid" validate:"required,printascii"`
+		gorm.Model `json:"-"` // model for managing record's crud datetime
 	}
 
 	// Locker is definition of Locker device which store devices with tag.
@@ -24,8 +24,8 @@ type (
 		RoomID     string         `json:"room_id" validate:"required,printascii"`                      // id of room where locker exist in
 		Security   LockerSecurity `json:"-" gorm:"embedded"`                                           // security data
 		Status     LockerStatus   `json:"status" gorm:"embedded"`                                      // locker's status
-		Tags       []Tag          `json:"tags" gorm:"foreignkey:LockerUID"`                            // Slice of tags which are stored in locker
-		gorm.Model                // model for managing record's crud datetime
+		Tags       []Tag          `json:"tags" gorm:"foreignkey:LockerUID;references:UID"`             // Slice of tags which are stored in locker
+		gorm.Model `json:"-"`     // model for managing record's crud datetime
 	}
 
 	// LockerSecurity is security data like credential, certificate for communicating
