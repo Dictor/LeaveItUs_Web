caa96f105a500fc977a382d05c9ad265a4343996
Dictor <kimdictor@gmail.com>
2020-10-24 10:20:50 +0000

    라커 이벤트 요청 모델 추가

--- backend/requestModel.go
+++ backend/requestModel.go
@@ -15,4 +15,16 @@ type (
 	LockerDeleteRequest struct {
 		UIDs []string `json:"uids" validate:"required,printascii"`
 	}
+
+	// LockerRecordCreateRequest is definition of POST locker recored request's body
+	LockerRecordCreateRequest struct {
+		TagUIDs *[]string `json:"tag_uids" validate:"required"`
+		Weight  float32   `json:"weight" validate:"required,numeric"`
+	}
+
+	// LockerDoorEventCreateRequest is definition of POST locker door event request's body
+	LockerDoorEventCreateRequest struct {
+		ClosedTime int `json:"close_time" validate:"numeric"`
+		Duration   int `json:"duration" validate:"numeric"`
+	}
 )
